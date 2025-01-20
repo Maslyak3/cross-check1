2a46066dd825eadd18f01615cbcc4bfbd5e0852f
morven2018 <you@example.com>
2025-01-21 01:10:06 +0300

    fix: update clear

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,29 +1,49 @@
 const fs = require('fs/promises');
+const { error } = require('node:console');
 const path = require('node:path');
 
 const getFolder = (nameFolder) =>
   path.join(__dirname, '..', '04-copy-directory', nameFolder);
+const initialPath = path.join(__dirname, '..', '04-copy-directory');
 
 const getPath = (currPath, name) => path.join(currPath, name);
 
 const folder = 'files';
 const folderCopy = 'files-copy';
 
-makeDir(getFolder(folderCopy));
+fs.mkdir(getFolder(folderCopy), { recursive: true }).then(() => {
+  console.log('Directory created successfully!');
+  makeCopy(getFolder(folder), getFolder(folderCopy));
+  console.log('Copy is ready');
+});
 
-function makeDir(path) {
-  fs.mkdir(path, { recursive: true }).then((dir) => {
-    if (!dir) {
-      fs.readdir(path, { withFileTypes: true }).then((filenames) => {
+function makeCopy(pathFolder, pathCopy) {
+  fs.mkdir(pathCopy, { recursive: true }).then((dir) => {
+    fs.readdir(pathCopy, { withFileTypes: true }).then((filenames) => {
+      if (!filenames.length) {
+        copyFolder(getFolder(folder), getFolder(folderCopy));
+      } else {
         filenames.forEach((element) => {
-          if (element.isDirectory()) {
-            fs.rm(getPath(path, element.name), { recursive: true }).then(() =>
-              copyFolder(getFolder(folder), getFolder(folderCopy)),
-            );
-          } else fs.unlink(getPath(path, element.name)).then(() => {});
+          if (element.isDirectory())
+            fs.rm(getPath(element.path, element.name), {
+              recursive: true,
+            }).then(() => copyFolder(getFolder(folder), getFolder(folderCopy)));
+          else {
+            if (element.path === getFolder(folderCopy)) {
+              fs.unlink(
+                getPath(element.path, element.name),
+                {
+                  recursive: true,
+                },
+                (error) => {
+                  console.log('no such', element.name);
+                },
+              );
+            }
+          }
         });
-      });
-    }
+      }
+    });
   });
 }
 
@@ -32,7 +52,10 @@ function copyFolder(folder, folderCopy) {
     .then((filenames) => {
       filenames.forEach((element) => {
         if (element.isDirectory()) {
-          makeDir(getPath(folderCopy, element.name));
+          makeCopy(
+            getPath(folder, element.name),
+            getPath(folderCopy, element.name),
+          );
           copyFolder(
             getPath(folder, element.name),
             getPath(folderCopy, element.name),
